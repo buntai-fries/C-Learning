8ddf38b5b7b937928f1565bb1c9e7f2fbd6f7e6b
buntai-fries <anuphmm2005@gmail.com>
2024-11-17 17:24:05 +0545

    Add second cpp file.

---
+++ var.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+    int x = 1; //initialize; no input
+    int y = 2; //initialize; no input
+    int z = x + y; 
+    std::cout << z; //print the result.
+    return 0;
+}
